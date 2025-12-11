cb7e2a532f64dc38a218c49ff1b29488ee693304
adamcockell1 <adam.cockell1@gmail.com>
2025-12-10 20:14:25 -0500

    🚧 Navbar redesign WIP

--- src/app.js
+++ src/app.js
@@ -4,31 +4,29 @@ const navBar = document.querySelector('.navBar');
 const navButtons = document.getElementsByClassName('navButton');
 const main = document.querySelector('.main');
 
-/* Theme and color button functions */
+/* Settings functions */
 
 let filters = ['invert(0)', 'grayscale(0)'];
 
-function theme() {
+function toggleTheme() {
     if (filters.includes('invert(1)')) {
         filters[0] = 'invert(0)';
     } else {
         filters[0] = 'invert(1)';
     }
-    // Overwrite currently applied filters with what is stored in the filters list
     document.documentElement.style.filter = filters.join(' ');
 }
 
-function color() {
+function toggleColor() {
     if (filters.includes('grayscale(1)')) {
         filters[1] = 'grayscale(0)';
     } else {
         filters[1] = 'grayscale(1)';
     }
-    // Overwrite currently applied filters with what is stored in the filters list
     document.documentElement.style.filter = filters.join(' ');
 }
 
-/* Scroll button functions */
+/* Scroll functions */
 
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
